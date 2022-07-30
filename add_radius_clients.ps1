128603e596318495febfd29e22dd980f86488c0f
s0apye <96381838+s0apye@users.noreply.github.com>
2022-07-30 20:10:09 +0100

    Update add_radius_clients.ps1

--- add_radius_clients.ps1
+++ add_radius_clients.ps1
@@ -1,4 +1,11 @@
-Import-csv "C:\AP.csv" | ForEach-Object 
-{
- New-NpsRadiusClient -Address $_.IP -Name $_.Name -SharedSecret $_.SecretPass
- }
+Write-Host 'Records below will be added as RADIUS clients'
+Import-csv -path "C:\AP.csv" | Format-Table
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+Import-csv "C:\AP.csv" | ForEach-Object {
+New-NpsRadiusClient -Address $_.IP -Name $_.Name -SharedSecret $_.SecretPass -AuthAttributeRequired $true }
+
+Write-Host -NoNewLine 'Operation successfull, press any key to exit.';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
